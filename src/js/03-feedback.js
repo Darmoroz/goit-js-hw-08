fbf7ffb7ebffa2445c0978f605b984b1adc31c07
Darmoroz <darmoroz99@gmail.com>
2022-11-05 23:00:51 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('.feedback-form input'),
+  textareaMessage: document.querySelector('.feedback-form textarea'),
+};
+let storage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (!storage) {
+  storage = {};
+} else {
+  if (storage.email) {
+    refs.inputEmail.value = storage.email;
+  }
+  if (storage.message) {
+    refs.textareaMessage.value = storage.message;
+  }
+}
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(storage);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  storage = {};
+}
+
+function onFormInput(e) {
+  storage[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(storage));
+}
